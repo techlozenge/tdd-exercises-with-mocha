8e1254ec1cabedec3509a1fe3da90db3f677ab1e
Dan Ford <daniel.ford@libertymutual.com>
2017-07-07 16:25:06 -0400

    completed tests for all assignments

--- addl_exercises.js
+++ addl_exercises.js
@@ -1,5 +1,3 @@
-  // NOTE: The Number.isNaN() method returns true if the value is of type Number otherwise it returns false
-
   function sum(a, b) {
     if (typeof(a) === 'number' && typeof(b) === 'number') {
       var x = (a + b).toFixed(0);
@@ -31,12 +29,18 @@
   }
 
   function secondHighest(nmbrArray) {
+    if (nmbrArray.length == 0) {
+      return -1;
+    }
     let highest = Math.max.apply(null, nmbrArray);
     nmbrArray.splice(nmbrArray.indexOf(highest), 1);
     return Math.max.apply(null, nmbrArray);
   }
 
   function containsVowel(str) {
+    if (str.length == 0) {
+      return -1;
+    }
     let vowel = ["a", "e", "i", "o", "u"];
     for (let i = 0; i < vowel.length; i++) {
       for (let x = 0; x < str.length; x++) {
@@ -48,14 +52,11 @@
     return false;
   }
 
-  // this function does work (as does longestWord) but it has a small bug where 'undefined' pops up and i don't know why!
-    //   Come now good day
-    // [ 'Come', 'now', 'good', 'day' ]
-    // omeCay ownay oodgay ayday
-    // undefined   <=== I don't see where the hell this is coming from!
   function pigLatin(str) {
-    console.log(str);
-    let wordArray = str.split(" ");    // split my string into an array of words
+    if (str.length == 0) {
+      return -1;
+    }
+    let wordArray = str.split(" ");
     for (let i = 0; i < wordArray.length; i++) {
         let plSuffix = wordArray[i].charAt(0) + "ay";
         wordArray[i] = wordArray[i].slice(1, wordArray[i].lrngth) + plSuffix;
@@ -64,12 +65,14 @@
     for (let z = 0; z < newStr.length; z++) {
       newStr = newStr.replace(",", " ");
     }
+    return newStr;
   }
 
   function longestWord(str) {
-    console.log(str);
-    let wordArray = str.split(" ");    // split my string into an array of words
-    console.log(wordArray);
+    if (str.length == 0) {
+      return -1;
+    }
+    let wordArray = str.split(" ");
     let lastVal = 0;
     let lastWord = " ";
     for (let i = 0; i < wordArray.length; i++) {
@@ -78,35 +81,8 @@
         lastWord = wordArray[i];
       }
     }
+    return lastWord;
   }
-
-  // let a = 10;
-  // let b = 11;
-  // let c = 12;
-  //
-  // let nmbrArray = [8, 3, 5, 1, 9, 10];
-  //
-  // //         012345678901234567890123456
-  // let str1 = "Ds ths strng cntan ny vwls?";
-  // let str2 = "Ds ths strng cntn ny vwls?";
-  // let str3 = "Come now good day";
-  // let str4 = "Evil prospers while good men do nothing.";
-  //
-  // console.log("Sum of " + a + " and " + b + " is " + " " + sum(a, b));
-  // console.log(" ");
-  // console.log("The average of " + a + ", " + b + ", and " + c + " is " + avg(a, b, c));
-  // console.log(" ");
-  // console.log("Is " + a + " greater than " + b + "? " + greaterThan(a, c));
-  // console.log(" ");
-  // console.log("The second highest number of [8, 3, 5, 1, 9, 10] is " + secondHighest(nmbrArray));
-  // console.log(" ");
-  // console.log (str1 + " " + containsVowel(str1));
-  // console.log (str2 + " " + containsVowel(str2));
-  // console.log(" ");
-  // console.log(pigLatin(str3));
-  // console.log(" ");
-  // console.log(longestWord(str4));
-  // console.log(" ");
 
 module.exports = {
   sum,
--- test/addl_exercises.js
+++ test/addl_exercises.js
@@ -54,9 +54,82 @@ describe('addlExercises', function() {
     });
 
     context('When sent an array of numbers [8, 3, 5, 1, 9, 10]', function() {
-         it('secondHighest returns the 2nd highest value in the array', function() {
+         it('secondHighest returns the 2nd highest value in the array, 9', function() {
             var res = addlExercises.secondHighest( [8, 3, 5, 1, 9, 10] );
             expect(res).to.equal(9);
+        });
+    });
+
+    context('When sent an array of numbers [8, 8, 8, 8, 8, 8]', function() {
+         it('secondHighest returns the 2nd highest value in the array, 8', function() {
+            var res = addlExercises.secondHighest( [8, 8, 8, 8, 8, 8] );
+            expect(res).to.equal(8);
+        });
+    });
+
+    context('When sent an empty array []', function() {
+         it('secondHighest returns -1 for failure', function() {
+            var res = addlExercises.secondHighest([]);
+            expect(res).to.equal(-1);
+        });
+    });
+
+    context('When sent a string of values that contains a vowel', function() {
+         it('containsVowel returns true if the string contains a vowel', function() {
+            var str = "Ds ths strng cntan ny vwls?";
+            var res = addlExercises.containsVowel(str);
+            expect(res).to.equal(true);
+        });
+    });
+
+    context('When sent a string of values that does NOT contain a vowel', function() {
+         it('containsVowel returns false', function() {
+            var str = "Ds ths strng cntn ny vwls?";
+            var res = addlExercises.containsVowel(str);
+            expect(res).to.equal(false);
+        });
+    });
+
+    context('When sent an empty string', function() {
+         it('containsVowel returns -1', function() {
+            var str = "";
+            var res = addlExercises.containsVowel(str);
+            expect(res).to.equal(-1);
+        });
+    });
+
+    // Come now good day
+    // [ 'Come', 'now', 'good', 'day' ]
+    // omeCay ownay oodgay ayday
+    context('When sent a string of values', function() {
+         it('pigLatin returns the string in pig latin format', function() {
+            var str = "Come now good day";
+            var res = addlExercises.pigLatin(str);
+            expect(res).to.equal("omeCay ownay oodgay ayday");
+        });
+    });
+
+    context('When sent an empty string', function() {
+         it('pigLatin returns -1', function() {
+            var str = "";
+            var res = addlExercises.pigLatin(str);
+            expect(res).to.equal(-1);
+        });
+    });
+
+    context('When sent a string of values', function() {
+         it('longestWord returns the longest word in the string', function() {
+            var str = "now is the time for all good men to come to the aid of their country";
+            var res = addlExercises.longestWord(str);
+            expect(res).to.equal("country");
+        });
+    });
+
+    context('When sent an empty string', function() {
+         it('longestWord returns -1', function() {
+            var str = "";
+            var res = addlExercises.longestWord(str);
+            expect(res).to.equal(-1);
         });
     });
 
